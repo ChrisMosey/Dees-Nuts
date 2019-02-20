c07639af37e46d123ec167c6af6a1dcc2ae52798
dan <daniel@danielherbert.ca>
2019-02-19 20:34:06 -0500

    acorn trigger

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private int speed = 10;
-
+    private int numAcorns = 0;
 
     // Use this for initialization
-    void Start()
-    {
-
+    void Start() {
+        Debug.Log("woooo doooooog");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Rigidbody rb = GetComponent<Rigidbody>();
-        Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    // void Update()
+    // {
+    //     Rigidbody rb = GetComponent<Rigidbody>();
+    //     Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-        rb.transform.position += Movement * speed * Time.deltaTime;
+    //     rb.transform.position += Movement * speed * Time.deltaTime;
 
 
-        Physics.gravity = new Vector3(0f, -100f, 0f);
+    //     Physics.gravity = new Vector3(0f, -100f, 0f);
+    // }
+
+    private void OnTriggerEnter(Collider other) {
+
+        if(other.name == "Acorn"){
+            numAcorns++;
+            Debug.Log(numAcorns);
+        }
+        
     }
+
+}
