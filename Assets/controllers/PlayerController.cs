a577ad9ae68c233f42260126cbac6a30d8636a67
dan <daniel@danielherbert.ca>
2019-02-23 09:53:50 -0500

    SEASONS BOIII

---
+++ Assets/controllers/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private int speed = 10;
+    private int numAcorns = 0;
+    public SeasonController seasonController;
+
+    // Use this for initialization
+    void Start() {
+        Debug.Log("NUTTTTS BOIIII");
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //     Rigidbody rb = GetComponent<Rigidbody>();
+    //     Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+    //     rb.transform.position += Movement * speed * Time.deltaTime;
+
+
+    //     Physics.gravity = new Vector3(0f, -100f, 0f);
+    // }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.name == "Acorn") addAcorns(1);        
+    }
+
+    private void addAcorns(int num){
+        numAcorns += num;
+
+        // Temp for testing
+        if(numAcorns%2 == 0){
+            seasonController.setSeason("WINTER");
+        }else{
+            seasonController.setSeason("SUMMER");
+        }
+    }
+
+    public int getNumAcorns(){
+        return numAcorns;
+    }
+
+}
